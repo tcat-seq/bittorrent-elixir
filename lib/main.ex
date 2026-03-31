3957840f86baf9d3aad82dfd54588d792a2e0bb4
tcat seq <tcat.seq@gmail.com>
2026-03-31 13:10:13 -0500

    - fixing warning

--- lib/main.ex
+++ lib/main.ex
@@ -1,34 +1,45 @@
 defmodule CLI do
   def main(argv) do
-      case argv do
-          ["decode" | [encoded_str | _]] ->
-              # You can use print statements as follows for debugging, they'll be visible when running tests.
-              IO.puts(:stderr, "Logs from your program will appear here!")
+    case argv do
+      ["decode" | [encoded_str | _]] ->
+        # You can use print statements as follows for debugging, they'll be visible when running tests.
+        IO.puts(:stderr, "Logs from your program will appear here!")
 
-              # TODO: Uncomment the code below to pass the first stage
-              # decoded_str = Bencode.decode(encoded_str)
-              # IO.puts(Jason.encode!(decoded_str))
-          [command | _] ->
-              IO.puts("Unknown command: #{command}")
-              System.halt(1)
-          [] ->
-              IO.puts("Usage: your_program.sh <command> <args>")
-              System.halt(1)
-      end
+        # TODO: Uncomment the code below to pass the first stage
+        decoded_str = Bencode.decode(encoded_str)
+        IO.puts(Jason.encode!(decoded_str))
+
+      [command | _] ->
+        IO.puts("Unknown command: #{command}")
+        System.halt(1)
+
+      [] ->
+        IO.puts("Usage: your_program.sh <command> <args>")
+        System.halt(1)
+    end
   end
 end
 
 defmodule Bencode do
-    def decode(encoded_value) when is_binary(encoded_value) do
-        binary_data = :binary.bin_to_list(encoded_value)
-        case Enum.find_index(binary_data, fn char -> char == 58 end) do
-          nil ->
-            IO.puts("The ':' character is not found in the binary")
-          index ->
-            rest = Enum.slice(binary_data, index+1..-1)
-            List.to_string(rest)
-        end
-      end
+  def decode(encoded_value) when is_binary(encoded_value) do
+    binary_data = :binary.bin_to_list(encoded_value)
 
-    def decode(_), do: "Invalid encoded value: not binary"
+    case Enum.find_index(binary_data, fn char -> char == 58 end) do
+      nil ->
+        IO.puts("The ':' character is not found in the binary")
+
+      index ->
+        # RAS: added "//1" to address the "warning: negative steps are not supported
+        # in Enum.slice/2". This warning in Elixir v1.19.4 is part of a deprecation
+        # process that began in a previous version. To fix this, we need to explicitly
+        # use the first..last//step notation for ranges with negative steps instead
+        # of implicitly relying on the function's internal handling"
+
+        # rest = Enum.slice(binary_data, index+1..-1)
+        rest = Enum.slice(binary_data, (index + 1)..-1//1)
+        List.to_string(rest)
+    end
+  end
+
+  def decode(_), do: "Invalid encoded value: not binary"
 end
